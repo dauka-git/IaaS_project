1e719a3801dccd729b614bb0ee3f8d67bfab178c
dauka-git <marlambekovdaulet@gmail.com>
2025-07-29 21:52:08 +0500

    2.1

--- server/scripts/createAdmin.js
+++ server/scripts/createAdmin.js
@@ -6,7 +6,7 @@ const User = require('../models/User');
 const createAdminUser = async () => {
   try {
     // Connect to MongoDB
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
