4620df2b43417e483a3fe79710cb6a5bf11ebaf6
dauka-git <marlambekovdaulet@gmail.com>
2025-07-30 13:44:40 +0500

    3.3

--- python_services/main.py
+++ python_services/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # CORS setup remains the same
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("FRONTEND_URL")],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
